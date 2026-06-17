be92dca199634b649fe8148565f50f2bfe85d5f9
fabya0014-creator <fabya0014@gmail.com>
2026-06-17 17:29:49 -0400

    produtos.js

---
+++ produtos.js
@@ -0,0 +1,269 @@
+const produtos = [
+
+{
+    id: 1,
+    nome: "Guarda-Roupa Ph127 6 Portas",
+    categoria: "moveis",
+    marca: "PH",
+    preco: "R$ 2.038,80",
+    parcelas: "12x de R$ 169,90",
+    imagem: "img/produtos/guarda-roupa-ph127.jpg",
+    descricao: "Guarda-roupa PH127 6 portas com espelho."
+},
+
+{
+    id: 2,
+    nome: "Estofado Adapt Veludo",
+    categoria: "moveis",
+    preco: "R$ 1.798,80",
+    parcelas: "12x de R$ 149,90",
+    imagem: "img/produtos/estofado-adapt.jpg",
+    descricao: "Estofado Adapt revestido em veludo."
+},
+
+{
+    id: 3,
+    nome: "Conjunto Mesa Poeme com 6 Cadeiras",
+    categoria: "moveis",
+    preco: "R$ 1.078,80",
+    parcelas: "12x de R$ 89,90",
+    imagem: "img/produtos/mesa-poeme.jpg",
+    descricao: "Mesa Poeme com 6 cadeiras."
+},
+
+{
+    id: 4,
+    nome: "Churrasqueira Elétrica Mondial Grand Steak CH-05",
+    categoria: "eletroportateis",
+    marca: "Mondial",
+    preco: "R$ 139,50",
+    parcelas: "5x de R$ 27,90",
+    imagem: "img/produtos/churrasqueira-mondial.jpg",
+    descricao: "Churrasqueira elétrica Grand Steak CH-05."
+},
+
+{
+    id: 5,
+    nome: "Fritadeira Air Fryer Mondial AFN-40 Family 4L",
+    categoria: "eletroportateis",
+    marca: "Mondial",
+    preco: "R$ 219,50",
+    parcelas: "5x de R$ 43,90",
+    imagem: "img/produtos/airfryer-afn40.jpg",
+    descricao: "Air Fryer Mondial Family 4 litros."
+},
+
+{
+    id: 6,
+    nome: "Cooktop Mueller 5 Bocas Tripla Chama",
+    categoria: "cozinha",
+    marca: "Mueller",
+    preco: "R$ 834,00",
+    parcelas: "12x de R$ 69,50",
+    imagem: "img/produtos/cooktop-mueller.jpg",
+    descricao: "Cooktop Mueller 5 bocas tripla chama."
+},
+
+{
+    id: 7,
+    nome: "Fogão Realce Ares 4 Bocas",
+    categoria: "cozinha",
+    marca: "Realce",
+    preco: "R$ 753,60",
+    parcelas: "12x de R$ 62,90",
+    imagem: "img/produtos/fogao-realce-ares.jpg",
+    descricao: "Fogão Realce Ares 4 bocas preto."
+}
+
+];{
+    id: 8,
+    nome: "Colchão Casal Gazin D33",
+    categoria: "colchoes",
+    marca: "Gazin",
+    parcelas: "12x de R$ 189,90",
+    imagem: "img/produtos/colchao-gazin-d33.jpg",
+    descricao: "Colchão casal Gazin D33."
+},
+
+{
+    id: 9,
+    nome: "Cama Box Casal Gazin",
+    categoria: "colchoes",
+    marca: "Gazin",
+    parcelas: "12x de R$ 291,50",
+    imagem: "img/produtos/cama-box-gazin.jpg",
+    descricao: "Cama Box casal Gazin."
+},
+
+{
+    id: 10,
+    nome: "Sofá de Canto Retrátil",
+    categoria: "moveis",
+    parcelas: "12x de R$ 279,90",
+    imagem: "img/produtos/sofa-canto.jpg",
+    descricao: "Sofá de canto retrátil e reclinável."
+},
+
+{
+    id: 11,
+    nome: "Cozinha Compacta Itatiaia",
+    categoria: "moveis",
+    marca: "Itatiaia",
+    parcelas: "12x de R$ 269,90",
+    imagem: "img/produtos/cozinha-itatiaia.jpg",
+    descricao: "Cozinha compacta em aço."
+},
+
+{
+    id: 12,
+    nome: "Balcão de Cozinha Itatiaia",
+    categoria: "moveis",
+    marca: "Itatiaia",
+    parcelas: "12x de R$ 69,90",
+    imagem: "img/produtos/balcao-itatiaia.jpg",
+    descricao: "Balcão de cozinha Itatiaia."
+},
+
+{
+    id: 13,
+    nome: "Colchão Solteiro Gazin",
+    categoria: "colchoes",
+    marca: "Gazin",
+    parcelas: "12x de R$ 27,90",
+    imagem: "img/produtos/colchao-solteiro.jpg",
+    descricao: "Colchão solteiro Gazin."
+},
+
+{
+    id: 14,
+    nome: "Mesa de Jantar 6 Cadeiras",
+    categoria: "moveis",
+    parcelas: "12x de R$ 189,90",
+    imagem: "img/produtos/mesa-6-cadeiras-premium.jpg",
+    descricao: "Conjunto mesa com 6 cadeiras."
+},
+
+{
+    id: 15,
+    nome: "Rack com Painel para TV",
+    categoria: "moveis",
+    parcelas: "12x de R$ 359,90",
+    imagem: "img/produtos/rack-painel-tv.jpg",
+    descricao: "Rack com painel para TV."
+},
+
+{
+    id: 16,
+    nome: "Fogão Atlas 4 Bocas",
+    categoria: "cozinha",
+    marca: "Atlas",
+    parcelas: "12x de R$ 168,20",
+    imagem: "img/produtos/fogao-atlas-4b.jpg",
+    descricao: "Fogão Atlas 4 bocas."
+},
+
+{
+    id: 17,
+    nome: "Fogão Atlas 5 Bocas",
+    categoria: "cozinha",
+    marca: "Atlas",
+    parcelas: "12x de R$ 188,40",
+    imagem: "img/produtos/fogao-atlas-5b.jpg",
+    descricao: "Fogão Atlas 5 bocas."
+},{
+    id: 18,
+    nome: "Refrigerador Consul Frost Free 340L",
+    categoria: "refrigeradores",
+    marca: "Consul",
+    parcelas: "12x de R$ 299,90",
+    imagem: "img/produtos/refrigerador-consul-340l.jpg",
+    descricao: "Refrigerador Consul Frost Free 340 litros."
+},
+
+{
+    id: 19,
+    nome: "Refrigerador Electrolux Frost Free",
+    categoria: "refrigeradores",
+    marca: "Electrolux",
+    parcelas: "12x de R$ 349,90",
+    imagem: "img/produtos/refrigerador-electrolux.jpg",
+    descricao: "Refrigerador Frost Free Electrolux."
+},
+
+{
+    id: 20,
+    nome: "Lavadora Electrolux 15kg",
+    categoria: "lavadoras",
+    marca: "Electrolux",
+    parcelas: "12x de R$ 249,90",
+    imagem: "img/produtos/lavadora-electrolux-15kg.jpg",
+    descricao: "Lavadora automática 15 kg."
+},
+
+{
+    id: 21,
+    nome: "Lavadora Consul 12kg",
+    categoria: "lavadoras",
+    marca: "Consul",
+    parcelas: "12x de R$ 189,90",
+    imagem: "img/produtos/lavadora-consul-12kg.jpg",
+    descricao: "Lavadora automática 12 kg."
+},
+
+{
+    id: 22,
+    nome: "Micro-ondas Midea 20L",
+    categoria: "cozinha",
+    marca: "Midea",
+    parcelas: "12x de R$ 59,90",
+    imagem: "img/produtos/microondas-midea-20l.jpg",
+    descricao: "Micro-ondas Midea 20 litros."
+},
+
+{
+    id: 23,
+    nome: "Forno Elétrico Mondial",
+    categoria: "cozinha",
+    marca: "Mondial",
+    parcelas: "12x de R$ 74,90",
+    imagem: "img/produtos/forno-mondial.jpg",
+    descricao: "Forno elétrico Mondial."
+},
+
+{
+    id: 24,
+    nome: "Liquidificador Mondial Turbo",
+    categoria: "eletroportateis",
+    marca: "Mondial",
+    parcelas: "5x de R$ 19,90",
+    imagem: "img/produtos/liquidificador-mondial.jpg",
+    descricao: "Liquidificador Mondial Turbo."
+},
+
+{
+    id: 25,
+    nome: "Ventilador Mondial 40cm",
+    categoria: "climatizacao",
+    marca: "Mondial",
+    parcelas: "5x de R$ 29,90",
+    imagem: "img/produtos/ventilador-mondial.jpg",
+    descricao: "Ventilador de mesa 40 cm."
+},
+
+{
+    id: 26,
+    nome: "Smart TV 43 Polegadas",
+    categoria: "tv",
+    parcelas: "12x de R$ 199,90",
+    imagem: "img/produtos/smart-tv-43.jpg",
+    descricao: "Smart TV LED 43 polegadas."
+},
+
+{
+    id: 27,
+    nome: "Smart TV 50 Polegadas 4K",
+    categoria: "tv",
+    parcelas: "12x de R$ 289,90",
+    imagem: "img/produtos/smart-tv-50.jpg",
+    descricao: "Smart TV 4K 50 polegadas."
+},
